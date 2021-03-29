8dee36e0cbd4da3087c77a5e20c74311b341560a
bartinio <karenchakhalyan23@gmail.com>
2021-03-29 17:40:10 +0400

    some changes

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,4 @@
 import Commerce from '@chec/commerce.js';
 
 export default commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY);
+REACT_APP_CHEC_PUBLIC_KEY = pk_2532280d10a3a11791cb8f8f1d0e0cb67a82f2d7dacf5;
