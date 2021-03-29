d092a4de80379130de8d6bd7db32cc634a0696c9
Karen <karenchakhalyan23@gmail.com>
2021-03-29 22:43:59 +0400

    connected to Commercejs.com

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -2,26 +2,8 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './product/Product';
 import useStyles from './styles';
-const products = [
-	{
-		id: 1,
-		name: 'Shoes',
-		description: 'Running Shoes',
-		price: '$59.99',
-		image:
-			'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lightweight-shoe-roundup-6990-1581370694.jpg?crop=0.755xw:0.851xh;0.0769xw,0.00240xh&resize=640:*',
-	},
-	{
-		id: 2,
-		name: 'MacBook',
-		description: 'Apple MacBook',
-		price: '$599.99',
-		image:
-			'https://res-5.cloudinary.com/grover/image/upload/e_trim/c_limit,f_auto,fl_png8.lossy,h_1280,q_auto,w_1280/v1585067771/qu1hso8d0e0nrz8cnmgz.png',
-	},
-];
 
-const Products = () => {
+const Products = ({ products }) => {
 	const classes = useStyles();
 	return (
 		<main className={classes.content}>
--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,6 @@
 import Commerce from '@chec/commerce.js';
 
-export default commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY);
-REACT_APP_CHEC_PUBLIC_KEY = pk_2532280d10a3a11791cb8f8f1d0e0cb67a82f2d7dacf5;
+export const commerce = new Commerce(
+	process.env.REACT_APP_CHEC_PUBLIC_KEY,
+	true
+);
