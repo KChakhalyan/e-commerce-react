a6cc25fc219a5533065ea39d887114fe9d7f4e68
bartinio <karenchakhalyan23@gmail.com>
2021-03-30 16:01:48 +0400

    some fixes

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CartItem from './cartItem/CartItem';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './styles';
 
@@ -6,45 +7,49 @@ const Cart = ({ cart }) => {
 	const classes = useStyles();
 
 	const EmptyCart = () => {
-		return(<Typography varriant="subtitle1">
-			You have no items in your shopping cart, start adding some!
-		</Typography>)
+		return (
+			<Typography varriant="subtitle1">
+				You have no items in your shopping cart, start adding some!
+			</Typography>
+		);
 	};
 	const FilledCart = () => {
-			return(<>
-			<Grid container spacing={3}>
-				{cart.line_items.map((item) => (
-					<Grid item xs={12} sm={4} key={item.id}>
-						<div>{item.name}</div>
-					</Grid>
-				))}
-			</Grid>
-			<div className={classes.cardDetails}>
-				<Typography variant="h4">
-					Subtotal: {cart.subtotal.formatted_with_symbol}
-				</Typography>
-				<div>
-					<Button
-						className={classes.emptyButton}
-						size="large"
-						type="button"
-						variant="contained"
-						color="secondary"
-					>
-						Empty Cart
-					</Button>
-					<Button
-						className={classes.checkoutButton}
-						size="large"
-						type="button"
-						variant="contained"
-						color="primary"
-					>
-						Checkout
-					</Button>
+		return (
+			<>
+				<Grid container spacing={3}>
+					{cart.line_items.map((item) => (
+						<Grid item xs={12} sm={4} key={item.id}>
+							<CartItem item={item} />
+						</Grid>
+					))}
+				</Grid>
+				<div className={classes.cardDetails}>
+					<Typography variant="h4">
+						Subtotal: {cart.subtotal.formatted_with_symbol}
+					</Typography>
+					<div>
+						<Button
+							className={classes.emptyButton}
+							size="large"
+							type="button"
+							variant="contained"
+							color="secondary"
+						>
+							Empty Cart
+						</Button>
+						<Button
+							className={classes.checkoutButton}
+							size="large"
+							type="button"
+							variant="contained"
+							color="primary"
+						>
+							Checkout
+						</Button>
+					</div>
 				</div>
-			</div>
-		</>)
+			</>
+		);
 	};
 
 	if (!cart.line_items) return 'Loading ...';
